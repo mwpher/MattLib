2db7bcec3e0b54fc6d115ccc5cd59f5a5214adf5
Matthew Pherigo <hybrid120@gmail.com>
2014-03-03 11:49:49 -0600

    Initial commit

---
+++ matt.c
@@ -0,0 +1,56 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  matt.c
+ *
+ *    Description:  My little collection of useful functions I've made for myself
+ *
+ *        Version:  1.0
+ *        Created:  03/03/14 11:18:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Matthew Pherigo (mwp), hybrid120@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+
+#include "matt.h"
+
+int stringtoint(char* string)
+{
+	const char* err;
+	long long ll;
+	int answer;
+
+	ll = strtonum(string, LLONG_MIN, LLONG_MAX, &err);
+	if (err != NULL)
+	{
+		errno = 1;
+		fprintf(stderr, "strtonum error: %s\n", err);
+		return 0;
+	}
+
+	if (ll > INT_MAX)
+	{
+		errno = 1;
+		fprintf(stderr, "stringtoint: number too big for int! (Number was %lld)\n", ll);
+		return 0;
+	}
+	else if (ll < INT_MIN)
+	{
+		errno = -1;
+		fprintf(stderr, "stringtoint: number too big for int! (Number was %lld)\n", ll);
+		return 0;
+	}
+
+	answer = (int)ll;
+	errno = 0;
+	return answer;
+}
---
+++ matt.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  matt.h
+ *
+ *    Description:  My little collection of programs
+ *
+ *        Version:  1.0
+ *        Created:  03/03/14 11:18:03
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Matthew Pherigo (mwp), hybrid120@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+int stringtoint(char* string);
